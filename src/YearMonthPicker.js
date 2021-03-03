ca74b36148376d6015d515e9640f51284d0ae08d
Johnny Backlund <johnnybacklund@hotmail.com>
2021-03-03 10:19:01 +0100

    Added functionality to dropdown.

--- src/YearMonthPicker.js
+++ src/YearMonthPicker.js
@@ -1,16 +1,29 @@
-
+import React, {useState} from 'react';
 
 function YearMonthPicker() {
+  const [showDropdown, setShowdropdown] = useState(false);
+
   return (
-    <div class="dropdown">
-      <button onclick="myFunction()" class="dropbtn">Dropdown</button>
-      <div id="myDropdown" class="dropdown-content">
-        <a href="#">Link 1</a>
-        <a href="#">Link 2</a>
-        <a href="#">Link 3</a>
+    <div className="dropdown">
+    <button onClick={() => setShowdropdown(!showDropdown)} className="dropbtn">Month</button>
+      <div id="myDropdown" className={`dropdown-content ${showDropdown ? 'show' : ''}`}>
+        <a href="#">January</a>
+        <a href="#">February</a>
+        <a href="#">March</a>
+        <a href="#">April</a>
+        <a href="#">May</a>
+        <a href="#">June</a>
+        <a href="#">July</a>
+        <a href="#">August</a>
+        <a href="#">September</a>
+        <a href="#">Oktober</a>
+        <a href="#">November</a>
+        <a href="#">December</a>
       </div>
     </div>
   )
 }
+
+//https://www.w3schools.com/howto/howto_js_dropdown.asp//
 
 export default YearMonthPicker;
