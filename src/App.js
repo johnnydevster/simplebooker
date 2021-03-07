783c86900261eabcbc83f79355490650d51eef74
Johnny Backlund <johnnybacklund@hotmail.com>
2021-03-08 00:14:08 +0100

    Added highlighting of dates, arrow icons.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import YearMonthPicker from './YearMonthPicker';
 
 function App() {
@@ -9,15 +9,20 @@ function App() {
 
   const [showDropdown, setShowdropdown] = useState(false);
   const [chosenMonth, setChosenMonth] = useState(monthNames[new Date().getMonth()]);
+  const [chosenDate, setChosenDate] = useState();
 
   const getDaysInMonth = function(month, year) {
     return new Date(year, month, 0).getDate();
   };
   
   const dateArray = [];
-  for (var i = 1; i <= getDaysInMonth(3, 2021); i++) { //replace with chosen month and year
+  for (var i = 1; i <= getDaysInMonth(monthNames.indexOf(chosenMonth) + 1, 2021); i++) { //replace with chosen month and year
     dateArray.push(i);
   }
+
+  useEffect(() => {
+    console.log(chosenDate)
+  })
 
   return (
     <div className="simplebooker-main">
@@ -27,11 +32,12 @@ function App() {
         setShowdropdown={setShowdropdown}
         chosenMonth={chosenMonth}
         setChosenMonth={setChosenMonth}
+        setChosenDate={setChosenDate}
         />
       <div className="datenumbers-container">
-        {dateArray.map(item => {
+        {dateArray.map(date => {
           return (
-            <div className="datenumbers">{item}</div>
+            <div className={`datenumbers${parseInt(chosenDate, 10) === date ? ' highlight' : ''}`} onClick={(e) => {setChosenDate(e.target.innerHTML)}} >{date}</div>
           )
         })}
       </div>
--- src/YearMonthPicker.js
+++ src/YearMonthPicker.js
@@ -4,16 +4,21 @@ function YearMonthPicker(props) {
   
   return (
     <div className="buttons-container">
+      <div className="iconcontainer"><i className="fa fa-angle-left"></i></div>
       <div className="dropdown">
         <button onClick={() => props.setShowdropdown(!props.showDropdown)} className="dropbtn">{props.chosenMonth}</button>
         <div id="myDropdown" className={`dropdown-content ${props.showDropdown ? 'show' : ''}`}>
           <ul>
             {props.monthNames.map((month, index) => {
-              return <li key={index} value={index} onClick={(e) => props.setChosenMonth(props.monthNames[e.target.value])}>{month}</li>
+              return <li key={index} value={index} onClick={(e) => {
+                props.setChosenMonth(props.monthNames[e.target.value]);
+                props.setChosenDate(0)
+              }}>{month}</li>
             })}
           </ul>
         </div>
       </div>
+      <div className="iconcontainer"><i className="fa fa-angle-right"></i></div>
     </div>
   )
 }
