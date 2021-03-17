da7770d8e36f34bb19c11e7003492712e4f82a0b
Johnny Backlund <johnnybacklund@hotmail.com>
2021-03-17 22:29:37 +0100

    Worked on main booking grid system.

--- src/App.js
+++ src/App.js
@@ -9,19 +9,51 @@ function App() {
     bookableTimes.push(i);
   }
 
+  const activities = [
+    "Room 1",
+    "Room 2",
+    "Room 3",
+    "Room 4"
+  ]
 
   return (
     <div className="booking-main">
-      <div className="activity-header">
-        <span>Name of activity here</span>
+      <div className="headers-row">
+        <div className="activity-header">
+          <span>Name of activity here</span>
+        </div>
+        <div className="times-header">
+          { bookableTimes.map((time) => {
+            return <div className="timecell">{time}</div>
+          }) }
+        </div>
       </div>
-      <div className="times-header">
-        {bookableTimes.map((time) => {
-          return <div className="timecell">{time}</div>
-        })}
-      </div>
+      { activities.map(activity => {
+        return (
+          <div className="activity-container">
+            <div className="activity">
+              {activity}
+            </div>
+            <div className="time-row">
+              {bookableTimes.map((time) => {
+                return <div className="timecell"></div>
+              })}
+            </div>
+          </div>
+          )
+      }) }
     </div>
+
   );
 }
 
 export default App;
+
+/* <div className="activity-row">
+        {activities.map((activity) => {
+          return <div className="activity">{activity}</div>
+        })}
+      </div>
+      <div className="time-row">
+
+      </div> */
